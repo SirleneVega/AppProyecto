66d740c99b4ff739fbb256f44fc5e239de73e16f
Bryan Alberto Vargas Reyes <bryanbeto1234@gmail.com>
2020-05-25 22:09:56 -0600

    Cambio 10.09

--- AppProyecto/MasterPage.Master.cs
+++ AppProyecto/MasterPage.Master.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+//referencia
+using DAL;
+
 namespace AppProyecto
 {
      
     public partial class MasterPage : System.Web.UI.MasterPage
     {
-       
+        DistribuidoraPEntities entities;
         protected void Page_Load(object sender, EventArgs e)
         {
+            entities = new DistribuidoraPEntities();
             if (!HttpContext.Current.User.Identity.IsAuthenticated)
             {
                 Session["tipoUsuario"] = 0;
@@ -23,6 +27,29 @@ namespace AppProyecto
             }
             
         }
-        
+
+        public bool validarPantalla(int tipo, String pantalla)
+        {
+            try
+            {
+                if (tipo != 0)
+                {
+                    Vp_PantallaUsuario validar = this.entities.Vp_PantallaUsuario.FirstOrDefault(u => (u.idRol.Equals(tipo)
+                       & u.descripcionPantalla.Equals(pantalla)));
+                    if (validar != null)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
     }
 }
